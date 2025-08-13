8af2e66d1b24fa97f43f42f5c1214c4824b885fe
AldanaMajo <majoaldana178@gmail.com>
2025-08-12 21:34:03 -0600

    Entidad Persona (clase padre)

--- BreakingGym.EntidadesNegocioEN/PersonaEN.cs
+++ BreakingGym.EntidadesNegocioEN/PersonaEN.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace BreakingGym.EntidadesNegocioEN
 {
-    internal class PersonaEN
+    public abstract class PersonaEN
     {
+        protected int id {get; set; } // Identificador único de la persona
+        protected string nombre { get; set; } // Nombre de la persona
+        protected string apellido { get; set; } // Apellido de la persona
+        protected string celular { get; set; } // Número de celular de la persona
+        protected int idRol { get; set; } // Clave foránea hacia Rol
+        protected int idTipoDocumento { get; set; } // Clave foránea hacia TipoDocumento
+        protected int documento { get; set; } // Documento de identidad de la persona
+
+        protected PersonaEN(int id, string nombre, string apellido, string celular, int idRol, int idTipoDocumento, int documento)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.celular = celular;
+            this.idRol = idRol;
+            this.idTipoDocumento = idTipoDocumento;
+            this.documento = documento;
+        }
+        public abstract void Registrar();
+        public abstract void Eliminar();
+        public abstract void Modificar();
+        public abstract void Mostrar();
     }
+    
 }
