d08f238459c55ebb54e7f33e2aacd79ec729627a
AldanaMajo <majoaldana178@gmail.com>
2025-08-16 19:34:17 -0600

    Cambios en diseño de inscripcion y PersonaEN

--- BreakingGym.EntidadesNegocioEN/ClienteEN.cs
+++ BreakingGym.EntidadesNegocioEN/ClienteEN.cs
@@ -6,15 +6,8 @@ using System.Threading.Tasks;
 
 namespace BreakingGym.EntidadesNegocioEN
 {
-    public class ClienteEN
+    public class ClienteEN : PersonaEN
     {
-        public int Id { get; set; }
-        public int IdRol { get; set; }       // Clave foránea hacia Rol
-        public int IdTipoDocumento { get; set; } // Clave foránea hacia TipoDocumento
-        public string Documento { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-        public string Celular { get; set; }
-
+      
     }
 }
